fd88335febb7947941bd2f79d13442fbb15c937b
Charles H Martin, PhD <charlesmartin14@gmail.com>
2023-08-09 17:35:01 -0700

    save models every 1000 e steps

--- train.py
+++ train.py
@@ -190,10 +190,9 @@ def main(args):
 
         if (e + 1) % 1000 == 0:
             steps = torch.arange(len(train_acc)).numpy() * steps_per_epoch
-            torch.save(model.state_dict(), f"pytorch_model.{steps}.bin)")
+            torch.save(model.state_dict(), f"pytorch_model.{steps[-1]}.bin)")
 
-    steps = torch.arange(len(train_acc)).numpy() * steps_per_epoch
-    torch.save(model.state_dict(), f"pytorch_model.{steps}.bin")
+    torch.save(model.state_dict(), f"pytorch_model.last.bin")
 
             
 
